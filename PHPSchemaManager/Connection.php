5c5b00334848574c04619465839da3503ee03ffc
Thiago Melo <thiagomp@gmail.com>
2013-05-28 23:35:37 -0300

    - Complying with PSR-2

--- PHPSchemaManager/Connection.php
+++ PHPSchemaManager/Connection.php
@@ -1,62 +1,66 @@
 <?php
 namespace PHPSchemaManager;
 
-class Connection {
-  
-  protected $data;
-  
-  function __construct($connectionName = 'default') {
-    $this->data = array(
-      'dbms'           => null,
-      'username'       => null,
-      'password'       => null,
-      'hostname'       => null,
-      'port'           => null,
-      'driver'         => null,
-      'connectionName' => $connectionName,
-    );
-  }
-  
-  function __set($name, $value) {
-    if (array_key_exists($name, $this->data)) {
+class Connection
+{
 
-      $this->data[$name] = $value;
-      
+    protected $data;
+
+    public function __construct($connectionName = 'default')
+    {
+        $this->data = array(
+            'dbms'           => null,
+            'username'       => null,
+            'password'       => null,
+            'hostname'       => null,
+            'port'           => null,
+            'driver'         => null,
+            'connectionName' => $connectionName,
+        );
     }
-    else {    
-      
-      $trace = debug_backtrace();
-      trigger_error(
-        'Undefined property via __set(): *' . $name . '*' . 
-        ' in ' . $trace[0]['file'] .
-        ' on line ' . $trace[0]['line'],
-        E_USER_NOTICE
-      );
-      
-      return null;
+
+    public function __set($name, $value)
+    {
+        if (array_key_exists($name, $this->data)) {
+
+            $this->data[$name] = $value;
+
+        } else {
+
+            $trace = debug_backtrace();
+            trigger_error(
+                'Undefined property via __set(): *' . $name . '*' .
+                ' in ' . $trace[0]['file'] .
+                ' on line ' . $trace[0]['line'],
+                E_USER_NOTICE
+            );
+
+            return null;
+        }
     }
-  }
-  
-  function __get($name) {
-    if (array_key_exists($name, $this->data)) {
+ 
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->data)) {
+
+            return $this->data[$name];
+
+        }
       
-      return $this->data[$name];
-      
-    }
-      
-    $trace = debug_backtrace();
-    trigger_error(
-      'Undefined property via __get(): *' . $name . '*' .
-      ' in ' . $trace[0]['file'] .
-      ' on line ' . $trace[0]['line'],
-      E_USER_NOTICE
-    );
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined property via __get(): *' . $name . '*' .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE
+        );
     
-    return null;
+        return null;
 
-  }
-  
-  public function __toString() {
-    return $this->data['connectionName'];
-  }
+    }
+ 
+    public function __toString()
+    {
+        return $this->data['connectionName'];
+    }
 }
