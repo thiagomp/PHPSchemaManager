526c1bdc236878759f6ad6e9e82464bb5085c312
Thiago Melo <thiagomp@gmail.com>
2013-06-02 15:31:28 -0300

    - execute PHPUnit before coding standards checks

--- autotest/psmbuild.php
+++ autotest/psmbuild.php
@@ -1,5 +1,9 @@
 <?php
 
+// First, execute the tests
+$cmd = "phpunit --testsuite PHPSchemaManagerSuite";
+system($cmd);
+
 // TODO find a way to check if PSR2 coding standard is installed in the system
 $cmd = "phpcs --standard=PSR2 PHPSchemaManager";
 system($cmd);
