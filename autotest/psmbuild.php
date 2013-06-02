6fea4770ee8e3f81ce749d76d83aed7e54e878cc
Thiago Melo <thiagomp@gmail.com>
2013-06-02 15:32:42 -0300

    - check if the phpunit execution was successful

--- autotest/psmbuild.php
+++ autotest/psmbuild.php
@@ -2,7 +2,11 @@
 
 // First, execute the tests
 $cmd = "phpunit --testsuite PHPSchemaManagerSuite";
-system($cmd);
+$ret = system($cmd);
+
+if (!$ret) {
+    die("Unit Test failed");
+}
 
 // TODO find a way to check if PSR2 coding standard is installed in the system
 $cmd = "phpcs --standard=PSR2 PHPSchemaManager";
