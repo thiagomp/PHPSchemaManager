1a4fd09e945de4a040af903aad852a3aa9d7bb4f
Thiago Melo <thiagomp@gmail.com>
2013-06-22 19:45:35 -0300

    - adding arguments in the github call

--- autotest/smauto.bat
+++ autotest/smauto.bat
@@ -1,4 +1,4 @@
 @echo off
 rd /s /q PHPSchemaManager
-git clone -b development https://github.com/thiagomp/PHPSchemaManager.git PHPSchemaManager
+git clone -b %1% https://github.com/thiagomp/PHPSchemaManager.git PHPSchemaManager
 php PHPSchemaManager/autotest/psmbuild.php %1%
--- autotest/smauto.sh
+++ autotest/smauto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf PHPSchemaManager
-git clone -b development https://github.com/thiagomp/PHPSchemaManager.git PHPSchemaManager
+git clone -b $1 https://github.com/thiagomp/PHPSchemaManager.git PHPSchemaManager
 php PHPSchemaManager/autotest/psmbuild.php $1
