fef5f418fec7fed11ed53157f181d474e49b5586
Thiago Melo <thiagomp@gmail.com>
2013-06-22 19:16:43 -0300

    - added argument in the smauto script

--- autotest/smauto.bat
+++ autotest/smauto.bat
@@ -1,5 +1,4 @@
 @echo off
-set arg1=%1
 rd /s /q PHPSchemaManager
 git clone -b development https://github.com/thiagomp/PHPSchemaManager.git PHPSchemaManager
-php PHPSchemaManager/autotest/psmbuild.php %arg1%
+php PHPSchemaManager/autotest/psmbuild.php %1%
